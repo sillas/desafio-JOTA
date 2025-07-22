2ff9bf99464d0388dcd3c2037deb6c774c8fe0a7
sillas <deepsky.sill@gmail.com>
2025-07-22 16:22:23 -0300

    auste em load_from_s3

--- src/lambdas/classification/access_layer/s3_handler.py
+++ src/lambdas/classification/access_layer/s3_handler.py
@@ -7,25 +7,22 @@ from loggin_config import logger
 s3_client = boto3.client('s3')
 
 
-def load_from_s3(object_key: str) -> bool:
+def load_from_s3(filename: str) -> bool:
     """Baixa um arquivo .zip do S3 para o diretório /tmp do Lambda e o extrai.
 
-    :param object_key: O caminho completo (chave) para o arquivo .zip no bucket.
+    :param filename: O nome do arquivo .zip no bucket.
     :return bool: Se o modelo foi baixado do S3 e descompactado com sucesso em /temp.
     """
 
-    extract_path = '/tmp'
-    zip_filename = os.path.basename(object_key)
-    zip_filepath = os.path.join(extract_path, zip_filename)
-
     bucket_name = os.getenv('S3_BUCKET')
+    zip_filepath = f"/tmp/{filename}"
 
     try:
         s3_client.download_file(
-            bucket_name, f"spacy_model/{object_key}", zip_filepath)
+            bucket_name, f"spacy_model/{filename}", zip_filepath)
 
         with zipfile.ZipFile(zip_filepath, 'r') as zip_ref:
-            zip_ref.extractall(extract_path)
+            zip_ref.extractall("/tmp")
 
         os.remove(zip_filepath)
 
